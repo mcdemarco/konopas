08db4f72857edf466eebac74d567d6fec8a1c436
M. C. DeMarco <mcd@mcdemarco.net>
2015-02-10 18:40:23 -0500

    Adjusted index and manifest for caching

--- util/cron-check-update-manifest.sh
+++ util/cron-check-update-manifest.sh
@@ -3,17 +3,17 @@
 ts=`date '+%Y-%m-%d_%H:%M'`
 cm="konopas.appcache"
 tgt="data/program.js"
-tgtp="data/participants.js"
+tgtp="data/people.js"
 tm="$tgt.$ts"
 tmp="$tgtp.$ts"
 
 grenadine_prog_url="https://boskone52.grenadine.co/konopas/program.js"
-grenadine_part_url="https://boskone52.grenadine.co/konopas/program/participants.js"
+grenadine_peep_url="https://boskone52.grenadine.co/konopas/program/participants.js"
 
 cd /home/nesfa/nesfa.org/boskone/konopas/
 
 curl "$grenadine_prog_url" > "$tm" 2>/dev/null
-curl "$grenadine_part_url" > "$tmp" 2>/dev/null
+curl "$grenadine_peep_url" > "$tmp" 2>/dev/null
 
 if  ! diff "$tm" "$tgt" >/dev/null 2>&1  ||  ! diff "$tmp" "$tgtp" >/dev/null 2>&1 
 then
