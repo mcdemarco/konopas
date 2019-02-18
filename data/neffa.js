6df90b094945494adc41562160373342439221e4
M. C. DeMarco <>
2019-02-18 16:56:44 -0500

    Make minor edits to update message.

--- data/neffa.js
+++ data/neffa.js
@@ -1,6 +1,6 @@
 var program = [
-{"id":"ChromeSuxRox","title":"Please update for 2019!","tags":[],"date":"2019-04-12","time":"19:00","mins":"50","loc":["MS Rm. 103/104", "Middle School"],
+{"id":"ChromeSuxRox","title":"Please update for 2019! (Click here.)","tags":[],"date":"2019-04-12","time":"19:00","mins":"50","loc":["MS Rm. 103/104", "Middle School"],
 "people":[]
-,"desc":"If you can see this message, you need to access the app via a new URL: <a href='https://app.neffa.org/'><span color='tomato'>https://</span>app.neffa.org/</a>!"}];
+,"desc":"If you can see this message, you need to access the app via a new URL: <a href='https://app.neffa.org/'>https://app.neffa.org/</a>"}];
 
 var people = [];
