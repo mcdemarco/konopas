499cf6f4b487728bc50e13c42401888d6b720709
Peter Olszowka <polszowka@cargurus.com>
2020-08-20 09:04:42 -0400

    Parse KonOpas data from NASFIC2020 schedule google spreadsheet. -- WIP

---
+++ data/people.js
@@ -0,0 +1,261 @@
+var people = [{"id": 0, "name": "Sherwood Smith", "prog": 73}, {"id": 1, "name": "Eric Flint", "prog": 29}, {
+    "id": 2,
+    "name": "Gillian Polack",
+    "prog": 94
+}, {"id": 3, "name": "Laurence Raphael Brothers", "prog": 52}, {"id": 4, "name": "Anne Gray", "prog": 108}, {
+    "id": 5,
+    "name": "Kathryn Sullivan",
+    "prog": 60
+}, {"id": 6, "name": "Christine Taylor-Butler", "prog": 81}, {
+    "id": 7,
+    "name": "Marieke Nijkamp",
+    "prog": 103
+}, {"id": 8, "name": "Nino Cipri", "prog": 68}, {"id": 9, "name": "Phoebe North", "prog": [1]}, {
+    "id": 10,
+    "name": "Walter Parker",
+    "prog": [4]
+}, {"id": 11, "name": "Alison Scott", "prog": 19}, {"id": 12, "name": "Ben Yalow", "prog": 47}, {
+    "id": 13,
+    "name": "Doug Piero Carey",
+    "prog": 102
+}, {"id": 14, "name": "+ one more", "prog": 85}, {"id": 15, "name": "Raven Oak", "prog": 103}, {
+    "id": 16,
+    "name": "Jude-Marie Green",
+    "prog": 57
+}, {"id": 17, "name": "Valerie Estelle Frankel", "prog": 108}, {
+    "id": 18,
+    "name": "Leonard Richardson (Presenter)",
+    "prog": [8]
+}, {"id": 19, "name": "William S. Higgins", "prog": [9]}, {"id": 20, "name": "Alec Nevala-Lee", "prog": 90}, {
+    "id": 21,
+    "name": "Bill Higgins",
+    "prog": 85
+}, {"id": 22, "name": "+ two more", "prog": [9]}, {"id": 23, "name": "TBD", "prog": 91}, {
+    "id": 24,
+    "name": "Tom Smith",
+    "prog": 112
+}, {"id": 25, "name": "Lisa Garrison", "prog": 112}, {"id": 26, "name": "Dale Mazzoli", "prog": 112}, {
+    "id": 27,
+    "name": "Chris Garcia",
+    "prog": 112
+}, {"id": 28, "name": "Brian Herbert", "prog": 112}, {"id": 29, "name": "Kevin J. Anderson", "prog": 112}, {
+    "id": 30,
+    "name": "Marc Millis",
+    "prog": 112
+}, {"id": 31, "name": "Stephanie Law", "prog": 112}, {"id": 32, "name": "Eric Flynt", "prog": 112}, {
+    "id": 33,
+    "name": "(Presentation)",
+    "prog": [15]
+}, {"id": 34, "name": "Mary Anne Mohanraj", "prog": 98}, {"id": 35, "name": "Foz Meadows", "prog": 87}, {
+    "id": 36,
+    "name": "Isabel Schechter",
+    "prog": 94
+}, {"id": 37, "name": "Nicholas Whyte", "prog": 90}, {
+    "id": 38,
+    "name": "Sumana (brainwane) Harihareswara",
+    "prog": [16]
+}, {"id": 39, "name": "Chris Conway", "prog": [17]}, {"id": 40, "name": "Michael R. Underwood", "prog": 27}, {
+    "id": 41,
+    "name": "Wendy Van Camp",
+    "prog": [19]
+}, {"id": 42, "name": "Jennifer Blackstream", "prog": 73}, {
+    "id": 43,
+    "name": "Laurence Raphael Brothers + two more!",
+    "prog": [20]
+}, {"id": 44, "name": "Roberta Rogow", "prog": 57}, {"id": 45, "name": "(Workshop?)", "prog": 26}, {
+    "id": 46,
+    "name": "Jason Sanford",
+    "prog": 82
+}, {"id": 47, "name": "Guest of Honor: Stephanie Law", "prog": [23]}, {
+    "id": 48,
+    "name": "Amanda Makepeace",
+    "prog": 82
+}, {"id": 49, "name": "Rachel Quinlan", "prog": [23]}, {"id": 50, "name": "Ray VanTilburg", "prog": [23]}, {
+    "id": 51,
+    "name": "Bill Thomasson",
+    "prog": [24]
+}, {"id": 52, "name": "Bogi Tak\u00e1cs", "prog": [24]}, {"id": 53, "name": "Premee Mohamed", "prog": 99}, {
+    "id": 54,
+    "name": "Wendy S. Delmater",
+    "prog": [24]
+}, {"id": 55, "name": "Chris M. Barkley", "prog": 102}, {"id": 56, "name": "Merav Hofman", "prog": [27]}, {
+    "id": 57,
+    "name": "Misha Stone",
+    "prog": 64
+}, {"id": 58, "name": "Sarah Loch", "prog": 78}, {"id": 59, "name": "Sumana Harihareswara", "prog": 98}, {
+    "id": 60,
+    "name": "Ada Palmer",
+    "prog": 99
+}, {"id": 61, "name": "Alaya Dawn Johnson", "prog": 46}, {"id": 62, "name": "Julia Rios", "prog": 54}, {
+    "id": 63,
+    "name": "Martin L. Shoemaker",
+    "prog": [29]
+}, {"id": 64, "name": "Laura Resnick", "prog": [29]}, {"id": 65, "name": "Sue Francis", "prog": [29]}, {
+    "id": 66,
+    "name": "Steven H Silver",
+    "prog": 94
+}, {"id": 67, "name": "Cat Rambo", "prog": 85}, {"id": 68, "name": "Beth Meacham", "prog": 49}, {
+    "id": 69,
+    "name": "Joe Siclari",
+    "prog": 47
+}, {"id": 70, "name": "Valerie Valdes", "prog": 52}, {"id": 71, "name": "Andrea Hairston", "prog": 71}, {
+    "id": 72,
+    "name": "Chris \"Silver Spook\" Miller",
+    "prog": 87
+}, {"id": 73, "name": "Vikram Paralker +one more", "prog": [33]}, {
+    "id": 74,
+    "name": "Jeremy Holcomb",
+    "prog": 67
+}, {"id": 75, "name": "J Holtham", "prog": [36]}, {"id": 76, "name": "Victor Raymond", "prog": 98}, {
+    "id": 77,
+    "name": "Whitney \"Strix\" Beltr\u00e1n",
+    "prog": 67
+}, {"id": 78, "name": "Brandon Wilson", "prog": 95}, {"id": 79, "name": "Elsa Sjunneson", "prog": 103}, {
+    "id": 80,
+    "name": "John Wiswell",
+    "prog": 82
+}, {"id": 81, "name": "Mari Ness", "prog": 84}, {"id": 82, "name": "Deirdre Murphy", "prog": [41]}, {
+    "id": 83,
+    "name": "Alma Alexander",
+    "prog": 77
+}, {"id": 84, "name": "Farah Mendlesohn", "prog": 90}, {"id": 85, "name": "Jordan Kurella", "prog": 84}, {
+    "id": 86,
+    "name": "Leigh Harlen",
+    "prog": 78
+}, {"id": 87, "name": "Lucy Snyder", "prog": [42]}, {
+    "id": 88,
+    "name": "Dr. Nakeema Stefflbauer",
+    "prog": [45]
+}, {"id": 89, "name": "Gadi Evron + two or three more", "prog": [45]}, {
+    "id": 90,
+    "name": "Pablo Defendini",
+    "prog": [46]
+}, {"id": 91, "name": "Mike Chen", "prog": [46]}, {
+    "id": 92,
+    "name": "Lucy M. Snyder + one more",
+    "prog": [46]
+}, {"id": 93, "name": "Sue & Steve Francis", "prog": [47]}, {
+    "id": 94,
+    "name": "Kathyrn Sullivan",
+    "prog": [48]
+}, {"id": 95, "name": "LD Lewis", "prog": [48]}, {"id": 96, "name": "Dominik Parisien", "prog": [49]}, {
+    "id": 97,
+    "name": "Lucy A. Snyder",
+    "prog": [49]
+}, {"id": 98, "name": "Neil Clarke", "prog": [49]}, {"id": 99, "name": "Juanita Coulson", "prog": [50]}, {
+    "id": 100,
+    "name": "Aliette de Bodard",
+    "prog": 68
+}, {"id": 101, "name": "Oghenechovwe Donald Ekpeki", "prog": 95}, {
+    "id": 102,
+    "name": "Sara Felix",
+    "prog": [53]
+}, {"id": 103, "name": "Charles Payseur", "prog": 78}, {
+    "id": 104,
+    "name": "Valerie Estelle Frankel + two more",
+    "prog": [54]
+}, {"id": 105, "name": "Erin Underwood", "prog": 93}, {"id": 106, "name": "John Medaille", "prog": 82}, {
+    "id": 107,
+    "name": "one more",
+    "prog": [55]
+}, {"id": 108, "name": "Michael Grossberg", "prog": 102}, {"id": 109, "name": "Tom Jackson", "prog": 59}, {
+    "id": 110,
+    "name": "F. Paul Wilson",
+    "prog": 59
+}, {"id": 111, "name": "Sarah Hoyt", "prog": 59}, {"id": 112, "name": "(Reading)", "prog": 106}, {
+    "id": 113,
+    "name": "Cynthia Diamond",
+    "prog": [57]
+}, {"id": 114, "name": "D. H. Timpko", "prog": [57]}, {"id": 115, "name": "Tessa McFionn", "prog": [57]}, {
+    "id": 116,
+    "name": "Mike Gibson",
+    "prog": [58]
+}, {"id": 117, "name": "Jeff Keller", "prog": [58]}, {"id": 118, "name": "Becca Mandel", "prog": [58]}, {
+    "id": 119,
+    "name": "William H. Stoddard",
+    "prog": [59]
+}, {"id": 120, "name": "", "prog": [59]}, {"id": 121, "name": "Daniel M. Kimmel", "prog": [60]}, {
+    "id": 122,
+    "name": "Douglas Johnson",
+    "prog": [60]
+}, {"id": 123, "name": "Stephen Leigh", "prog": [60]}, {"id": 124, "name": "Cheryl Morgan", "prog": 107}, {
+    "id": 125,
+    "name": "Silvia Moreno-Garcia",
+    "prog": [61]
+}, {"id": 126, "name": "Vikram Paralkar", "prog": [61]}, {
+    "id": 127,
+    "name": "Maralisa Valtazanou",
+    "prog": [62]
+}, {"id": 128, "name": "Bill Lawhorn and Colette H. Fozard (Discon III)", "prog": [63]}, {
+    "id": 129,
+    "name": "Helen Montgomery (Chicon 8)",
+    "prog": [63]
+}, {"id": 130, "name": "Esther MacCallum-Stewart (Glasgow in 2024)", "prog": [63]}, {
+    "id": 131,
+    "name": "Day Al-Mohamed",
+    "prog": 103
+}, {"id": 132, "name": "Brian Herbert and Kevein J. Anderson", "prog": [65]}, {
+    "id": 133,
+    "name": "Naomi Hinchen",
+    "prog": [66]
+}, {"id": 134, "name": "Peggi Warner-Lalonde", "prog": 89}, {
+    "id": 135,
+    "name": "Phoebe Barton",
+    "prog": 85
+}, {"id": 136, "name": "dave ring", "prog": 107}, {"id": 137, "name": "Eboni Dunbar", "prog": 95}, {
+    "id": 138,
+    "name": "L.D. Lewis",
+    "prog": [68]
+}, {"id": 139, "name": "Tom Smith + one more", "prog": [70]}, {
+    "id": 140,
+    "name": "Gadi Evron",
+    "prog": [71]
+}, {"id": 141, "name": "Malka Older", "prog": [71]}, {"id": 142, "name": "Jim C. Hines", "prog": [73]}, {
+    "id": 143,
+    "name": "Steven Gould",
+    "prog": 81
+}, {"id": 144, "name": "Christopher J. Garcia", "prog": [74]}, {
+    "id": 145,
+    "name": "J. Holtham",
+    "prog": [74]
+}, {"id": 146, "name": "Anabel Graetz", "prog": 88}, {"id": 147, "name": "+one more", "prog": [77]}, {
+    "id": 148,
+    "name": "Freya Marske",
+    "prog": [78]
+}, {"id": 149, "name": "Eileen Gunn", "prog": 98}, {"id": 150, "name": "Ace Ratcliff", "prog": 103}, {
+    "id": 151,
+    "name": "Caitlin Starling",
+    "prog": [84]
+}, {"id": 152, "name": "Jeannette Ng", "prog": [84]}, {"id": 153, "name": "+one or two more", "prog": [86]}, {
+    "id": 154,
+    "name": "+ one more?",
+    "prog": [87]
+}, {"id": 155, "name": "Merav Hoffman", "prog": [88]}, {"id": 156, "name": "Errick Nunnally", "prog": 93}, {
+    "id": 157,
+    "name": "James Bacon and Christopher J. Garcia",
+    "prog": 93
+}, {"id": 158, "name": "Daniel Kimmel", "prog": [94]}, {"id": 159, "name": "Dan Tres Omi", "prog": [95]}, {
+    "id": 160,
+    "name": "Sheree Ren\u00e9e Jones",
+    "prog": [95]
+}, {"id": 161, "name": "(demo)", "prog": [96]}, {
+    "id": 162,
+    "name": "Sara Felix (demonstration)",
+    "prog": [96]
+}, {"id": 163, "name": "(Kaffeeklatch)", "prog": [97]}, {
+    "id": 164,
+    "name": "Tobias S. Buckell",
+    "prog": 108
+}, {"id": 165, "name": "Amy McNally", "prog": [100]}, {"id": 166, "name": "Red Kovatch", "prog": [101]}, {
+    "id": 167,
+    "name": "Lizzy Gibson",
+    "prog": [101]
+}, {"id": 168, "name": "Joel Zakem", "prog": [102]}, {"id": 169, "name": "Julie Washington", "prog": [102]}, {
+    "id": 170,
+    "name": "Neil Clark",
+    "prog": [107]
+}, {"id": 171, "name": "Pablo Defendini + one more?", "prog": [107]}, {
+    "id": 172,
+    "name": "Jamar Nicholas",
+    "prog": [108]
+}];
---
+++ util/lib/gdrive2json.php
@@ -0,0 +1,110 @@
+<?php
+
+/*  Google Drive Spreadsheet -> JSON converter
+ *  Copyright (c) 2013 by Eemeli Aro <eemeli@gmail.com>
+ *
+ *
+ *  Fetches a non-private Google Drive/Docs spreadsheet as CSV and converts it
+ *  to JSON. In field names, a "." indicates sub-objects; use zero-indexed
+ *  entries to generate arrays rather than objects.
+ *
+ *  EXAMPLE USAGE:
+
+      <?php
+      $key = preg_replace('/\W/', '', @$_GET['key']);
+      $gid = isset($_GET['gid']) ? preg_replace('/\D/', '', $_GET['gid']) : '0';
+
+      require_once('gdrive2json.php');
+
+      header("Content-type: application/json; charset=UTF-8;");
+      echo gdrive2json($key, $gid);
+
+ *  Permission to use, copy, modify, and/or distribute this software for any
+ *  purpose with or without fee is hereby granted, provided that the above
+ *  copyright notice and this permission notice appear in all copies.
+ *
+ *  The software is provided "as is" and the author disclaims all warranties
+ *  with regard to this software including all implied warranties of
+ *  merchantability and fitness. In no event shall the author be liable for
+ *  any special, direct, indirect, or consequential damages or any damages
+ *  whatsoever resulting from loss of use, data or profits, whether in an
+ *  action of contract, negligence or other tortious action, arising out of
+ *  or in connection with the use or performance of this software.
+ *
+ */
+
+
+require_once('url_fetch.php');
+require_once('parsecsv.lib.php');
+
+function gdrive2json($key, $gid = '0') {
+    if (!$key) exit("'key' parameter is required.");
+
+    //$url = "https://docs.google.com/spreadsheets/d/$key/export?format=csv&id=$key&gid=404267302";
+    //$url = "https://docs.google.com/spreadsheets/d/$key/gviz/tq?tqx=out:csv&sheet=404267302";
+    $dest = array();
+    $prog_item_id = 0;
+    $all_people = array();
+    $all_people_id = 0;
+    $url_array = array();
+    $url_array[] = "https://docs.google.com/spreadsheets/d/e/$key/pub?output=csv&gid=672691432";
+    $url_array[] = "https://docs.google.com/spreadsheets/d/e/$key/pub?output=csv&gid=404267302";
+    $url_array[] = "https://docs.google.com/spreadsheets/d/e/$key/pub?output=csv&gid=1067836657";
+    foreach ($url_array as $url) {
+
+        $rc = url_fetch($url, $csv_str);
+        if ($rc) exit("URL fetch error: $rc");
+    
+        $csv = new parseCSV("$csv_str\n");
+        $a = $csv->data;
+        foreach ($a as $source_row) {
+            if ($source_row['Day'] != '') {
+                $dest_row = array();
+                $pid = $prog_item_id++;
+                $dest_row['id'] = $pid;
+                $dest_row['title'] = $source_row['Title [All Times Eastern Daylight Time]'];
+                $dest_row['desc'] = isset($source_row['Panel Description']) ? $source_row['Panel Description'] : $source_row['Event Description'];
+                $dest_row['loc'] = array($source_row['Room']);
+                $people_names = array();
+                $moderator = $source_row['Moderator'];
+                $people = array();
+                if ($moderator != '' && substr($moderator, 1, 1) != '(' && $moderator != 'Precorded') {
+                    $moderators = explode(',', $moderator);
+                    foreach ($moderators as $mod) {
+                        $mod2 = trim($mod);
+                        if (isset($all_people[$mod2])) {
+                            $all_people[$mod2]['prog'] = $pid;
+                        } else {
+                            $all_people[$mod2] = array('id' => $all_people_id++, 'prog' => array($pid));
+                        }
+                        $people[] = array('id' => $all_people[$mod2]['id'], 'name' => $mod2.' (moderator)');
+                    }
+                }
+                $participant = $source_row['Panelists / Presenters'];
+                if ($participant != '') {
+                    $participants = explode(',', $participant);
+                    foreach ($participants as $part) {
+                        $part2 = trim($part);
+                        if (isset($all_people[$part2])) {
+                            $all_people[$part2]['prog'] = $pid;
+                        } else {
+                            $all_people[$part2] = array('id' => $all_people_id++, 'prog' => array($pid));
+                        }
+                        $people[] = array('id' => $all_people[$part2]['id'], 'name' => $part2);
+                    }
+                }
+                if (count($people) > 0) {
+                    $dest_row['people'] = $people;
+                }
+                $dest[] = $dest_row;
+            }
+        }
+    }
+    $programjs = json_encode($dest);
+    $dest_people = array();
+    foreach ($all_people as $name => $person) {
+        $dest_people[] = array('id' => $person['id'], 'name' => $name, 'prog' => $person['prog']);
+    }
+    $peoplejs = json_encode($dest_people);
+	return array('program' => $programjs, 'people' => $peoplejs);
+}
